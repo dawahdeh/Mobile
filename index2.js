00aa6245ccd5b946d856a5662cdacea8e948cc70
MohamadDawahdeh <>
2021-07-01 11:55:35 +0000

    updating

---
+++ index2.js
@@ -0,0 +1,15 @@
+alert("Welcome to mobile website ")
+
+
+
+var name =prompt("what is your fav mobile brand")
+
+if (name== "iphone")
+alert("your fav brand iphone ")
+
+else 
+alert("your fav brand is " + name)
+
+
+
+document.write("<h4>  Your fav brand is</h4> " +  name )
